949103438e192bd21f7fb9680f689dea1644fccf
Shantanu <shannformal323@gmail.com>
2026-03-26 17:42:42 +0530

    chore: cors set

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 import { ENV_VARS } from "./utils/env.js";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({ credentials: true, origin: ENV_VARS.CLIENT_URL }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
