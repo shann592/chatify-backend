cbc49f1210704e0030564cd84e02687348e1b4ba
Shantanu <shannformal323@gmail.com>
2026-03-30 20:28:23 +0530

    chore: request body limit added

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import { ENV_VARS } from "./utils/env.js";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: ENV_VARS.CLIENT_URL }));
 app.use("/api/auth", authRoutes);
